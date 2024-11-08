58b4f206315885af757b0386f0aad81a21e2ede7
Leong Zhe Cheng <zhecheng555@gmail.com>
2024-11-08 19:07:13 +0800

    added logic

--- test.py
+++ test.py
@@ -37,6 +37,7 @@ GPIO.setup(PIR_PIN, GPIO.IN)
 
 # Variable to store motion detection status
 motion_detected = False
+counter = 0
 
 def motion_sensor():
     global motion_detected
@@ -47,8 +48,6 @@ def motion_sensor():
         while True:
             if GPIO.input(PIR_PIN):
                 motion_detected = True
-                print("Motion Detected!")
-                mqttc.publish("g1g5.homeshield.levis.shopee", "motion", qos=1)
                 time.sleep(1)  # Delay to avoid multiple triggers
             else:
                 motion_detected = False
@@ -68,8 +67,12 @@ try:
     while True:
         if motion_detected:
             # Handle motion detected event
-            print("Handling motion event...")
+            print("Motion Detected!")
             motion_detected = False
+            counter += 1
+            if counter == 10:
+                mqttc.publish("g1g5.homeshield.levis.shopee", "motion", qos=1)
+                counter = 0
             # You can add any additional processing here
             time.sleep(5)  # Optional delay to avoid handling too fast
 
